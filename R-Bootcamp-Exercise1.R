9a0f2fd89f9678941f2d880058007948c619d1ed
hspeck <hspeck@ucla.edu>
2016-09-14 16:26:13 -0700

    second commit for Exercise1

--- R-Bootcamp-Exercise1.R
+++ R-Bootcamp-Exercise1.R
@@ -36,3 +36,50 @@ for(ii in 1:19){
     cat("&")
   }
 }
+#it works! yay
+
+
+#Exercise 3
+#dogs variable counts as 10, 5 iterations, will overwrite dog variable to add 1
+#at aka start dogs=10
+#at i=1 dogs=11
+#at i=2 dogs=12
+#at i=3 dogs=13
+#at i=4 dogs=14
+#at i=5 dogs=15 (end)
+
+#meatloaf assigned as 0 to start, 
+#for iterations 5-9 meatloaf is subtracting i and adding 1
+#afterward it cat (meatloaf) value
+#from experimentation there seems to be unstated parantheses around the stuff behind the assigner
+#at start meatloaf =0
+#at i=5 meatloaf=-4
+#at i=6 meatloaf=-9
+#at i=7 meatloaf=-15
+#at i=8 meatloaf=-22
+#at i=9 meatloaf=-30 (end)
+
+#bubbles starts at 12
+#it iterates through -1 to -4, going in that direction,
+#at each i value it reassigns value of bubbles to i
+#at start bubbles=12
+#at i=-1 bubbles=-1
+#at i=-2 bubbles=-2
+#at i=-3 bubbles=-3
+#at i=-4 bubbles=-4
+
+
+#Exercise 4
+#want to modify message, says to try with modulus
+#want to code so it gives message in presedential and congressional years (presendential years are divisible by 4, congressional ones are divisble by 2)
+#will need a separate if statement for presendential years, this should be %%4, the other should be %%2
+#only congressional when year divided by 4 has remainder 2 so use that to call it alone, and make separate message when divisible by 4 with r=0 for both of them
+years <- c( 2015, 2016, 2018, 2020, 2021)
+for(ii in 1:length(years)){
+  if(years[ii] %% 4 == 2){
+    cat(years[ii], 'Hooray, congressional elections!', sep = '\t', fill = T)
+  }
+  if(years[ii]%% 4==0){
+    cat(years[ii], "Hooray, presidential and congressional elections!", sep = '\t', fill=T)
+  }
+}
