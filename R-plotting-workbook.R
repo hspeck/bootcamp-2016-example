3ebdd817f411ed87792f168a3a7cfc58d60fde75
hspeck <hspeck@ucla.edu>
2016-09-21 19:34:46 -0700

    figured out the double barplot

--- R-plotting-workbook.R
+++ R-plotting-workbook.R
@@ -56,6 +56,18 @@ legend(6,350,c(expression(paste(sigma,"=3")),expression(paste(sigma,"=6"))),col=
 dev.off()
 #not working
 
+#made it work
+s1<-rnorm(1000,mean=0,sd=1)
+s3<-rnorm(1000,mean=0,sd=3)
+bins<-seq(-12,12,by=1)
+counts_s1<-hist(s1,breaks=bins)$counts
+counts_s3<-hist(s3,breaks=bins)$counts
+pdf(file="normal_barplot.pdf", width=6,height=6);
+par(mfrow=c(1,1), mar=c(4, 4, 3, 2))
+barplot(rbind(counts_s1,counts_s3),col=c(2,4), names.arg=seq(-12,11.5,by=1),beside=T,xlab="Value",ylab="Count")
+legend(6,350,c(expression(paste(sigma,"=3")),expression(paste(sigma,"=6"))),col=c(2,4),lwd=4)
+dev.off()
+
 mean(s1>1.96)
 mean(s1<0)
 s1<0
